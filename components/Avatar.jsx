6062d94ffe33ea17869daa1d078608b0310a9321
Mohammed Safwan <safwanpklr@gmail.com>
2023-02-25 15:25:57 +0530

    added avatar and name to the frontend

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,14 +1,11 @@
-export default function Avatar({ size }) {
+export default function Avatar({ size, url }) {
   let width = "w-12 h-12";
   if (size === "lg") {
     width = "md:w-36 md:h-36 w-24 h-24";
   }
   return (
     <div className={`${width} rounded-full overflow-hidden`}>
-      <img
-        src="https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
-        alt=""
-      />
+      <img src={url} alt="" />
     </div>
   );
 }
--- components/PostFormCard.jsx
+++ components/PostFormCard.jsx
@@ -1,16 +1,33 @@
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useEffect, useState } from "react";
 import Avatar from "./Avatar";
 import Card from "./Card";
 
 export default function PostFormCard() {
+  const [profile, setProfile] = useState(null);
+  const supabase = useSupabaseClient();
+  const session = useSession();
+  useEffect(() => {
+    // alert(session.user.id)
+    supabase
+      .from("profiles")
+      .select()
+      .eq("id", session.user.id)
+      .then((result) => {
+        if (result.data.length) {
+          setProfile(result.data[0]);
+        }
+      });
+  }, []);
   return (
     <Card>
       <div className="flex gap-2">
         <div>
-          <Avatar />
+          <Avatar url={profile?.avatar} />
         </div>
         <textarea
           className="grow p-3 h-14"
-          placeholder={"What's on your mind , USER ?"}
+          placeholder={`What's on your mind , ${profile?.name} ?`}
         />
       </div>
       <div className="flex gap-5 items-center mt-2">
