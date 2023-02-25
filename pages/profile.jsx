3adf6652381e26efb054c4862063767b388cc40f
Mohammed Safwan <safwanpklr@gmail.com>
2023-02-25 10:32:01 +0530

    completed profile page added photos friends about

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -168,6 +168,44 @@ export default function ProfilePage() {
           </Card>
         </div>
       )}
+      {isPhotos && (
+        <div>
+          <Card>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+                <img
+                  src="https://images.unsplash.com/photo-1598395927056-8d895e701c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3JlZWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+              </div>
+              <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+                <img
+                  src="https://images.unsplash.com/photo-1603565816030-6b389eeb23cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdyZWVjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+              </div>
+              <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+                <img
+                  src="https://plus.unsplash.com/premium_photo-1666621685703-d28e502abbd0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGdyZWVjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+              </div>
+              <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+                <img
+                  src="https://images.unsplash.com/photo-1616864068729-d086f91fd566?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGdyZWVjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+              </div>
+              <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+                <img
+                  src="https://images.unsplash.com/photo-1618500031461-a5fc01e96763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGdyZWVjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+              </div>
+            </div>
+          </Card>
+        </div>
+      )}
     </Layout>
   );
 }
