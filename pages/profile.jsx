364629433f036ac99aa23f6fd56aab0cb879e424
Mohammed Safwan <safwanpklr@gmail.com>
2023-02-27 20:22:34 +0530

    completed profile page view and update

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -11,6 +11,9 @@ import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
   const [profile, setProfile] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [name, setName] = useState("");
+  const [place, setPlace] = useState("");
 
   const router = useRouter();
   const session = useSession();
@@ -24,7 +27,7 @@ export default function ProfilePage() {
     fetchUser();
   }, [userId]);
 
-  const fetchUser = () => {
+  function fetchUser() {
     supabase
       .from("profiles")
       .select()
@@ -37,7 +40,31 @@ export default function ProfilePage() {
           setProfile(result.data[0]);
         }
       });
-  };
+  }
+
+  function saveProfile() {
+    supabase
+      .from("profiles")
+      .update({
+        name,
+        place,
+      })
+      .eq("id", session.user.id)
+      .then((result) => {
+        if (!result.error) {
+          setProfile((prev) => ({ ...prev, name, place }));
+
+          // setProfile((prev) => {
+          // const newProfile = prev;
+          // newProfile.name = name;
+          // newProfile.place = place;
+          // return newProfile;
+          // or
+          // });
+        }
+        setEditMode(false);
+      });
+  }
 
   const { asPath: pathname } = router;
   const isPosts = pathname.includes("posts") || pathname === "/profile";
@@ -71,9 +98,87 @@ export default function ProfilePage() {
             )}
           </div>
           <div className="p-4 pt-0 md:pt-4 pb-0">
-            <div className="ml-24 md:ml-40">
-              <h1 className="text-3xl font-bold">{profile?.name}</h1>
-              <div className="text-gray-500 leading-4">{profile?.place}</div>
+            <div className="ml-24 md:ml-40 flex justify-between">
+              <div>
+                {editMode && (
+                  <div>
+                    <input
+                      type="text"
+                      className="border py-2 px-3 rounded-md"
+                      placeholder={"Your Name"}
+                      onChange={(event) => setName(event.target.value)}
+                      value={name}
+                    />
+                  </div>
+                )}
+                {!editMode && (
+                  <h1 className="text-3xl font-bold">{profile?.name}</h1>
+                )}
+                {!editMode && (
+                  <div className="text-gray-500 leading-4">
+                    {profile?.place || "No Place Added"}
+                  </div>
+                )}
+                {editMode && (
+                  <div>
+                    <input
+                      type="text"
+                      className="border py-2 px-3 rounded-md mt-1"
+                      placeholder={"Your Location"}
+                      onChange={(event) => setPlace(event.target.value)}
+                      value={place}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="grow">
+                <div className="text-right">
+                  {isMyUser && !editMode && (
+                    <button
+                      onClick={() => {
+                        setEditMode(true);
+                        setName(profile.name);
+                        setPlace(profile.place);
+                      }}
+                      className="inline-flex gap-1 mx-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2 "
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                      Edit Profile
+                    </button>
+                  )}
+                  {isMyUser && editMode && (
+                    <button
+                      onClick={saveProfile}
+                      className="inline-flex gap-1 mx-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2"
+                    >
+                      Save Profile
+                    </button>
+                  )}
+                  {isMyUser && editMode && (
+                    <button
+                      onClick={() => {
+                        setEditMode(false);
+                      }}
+                      className="inline-flex gap-1 mx-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2"
+                    >
+                      Cancel
+                    </button>
+                  )}
+                </div>
+              </div>
             </div>
             <div className="mt-4 md:mt-10 flex gap-0">
               <Link
